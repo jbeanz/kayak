e0d0cc1db60c10bf51a17e78fcd120a5e092d202
jbeanz <jbeanzsoup@gmail.com>
2022-07-03 21:21:36 +0900

    add css, build searchBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import SearchBar from "./components/SearchBar";
+import SearchBar from "./components/Search/SearchBar";
 import NavBar from "./components/NavBar";
 import DestinationSlideshow from "./components/DestinationSlideshow";
 
 function App() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
   return (
     <div className="App">
+      <img
+        src="https://img.icons8.com/external-icongeek26-outline-colour-icongeek26/100/000000/external-space-shuttle-space-icongeek26-outline-colour-icongeek26.png"
+        alt="Kim Space Tour Logo"
+      />
+      <h1>KIM'S SPACE TOUR! </h1>
+      <h2>REACH NEW HEIGHTS</h2>
       {/* <NavBar /> */}
       <SearchBar />
-      <DestinationSlideshow />
+
+      {/* <DestinationSlideshow /> */}
     </div>
   );
 }
